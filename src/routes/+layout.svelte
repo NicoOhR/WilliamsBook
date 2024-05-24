3d94d30910c018092a0cafe0653255ea14a7a316
NicoOhR <nimoohayonr@gmail.com>
2024-05-23 23:37:00 -0500

    realizing this is going to take a long time

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
   <script type="text/javascript" id="MathJax-script" async
     src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js">
   </script>
+  <link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/dreampulse/computer-modern-web-font/master/fonts.css">
 
 </svelte:head>
 
--- src/routes/chapters/chapter1/+page.svelte
+++ src/routes/chapters/chapter1/+page.svelte
@@ -1,13 +1,31 @@
 <script>
     import { math, display } from 'mathlifier';
 </script>
+
+<head>
+    <title>Independence Posets</title>
+  <style>
+    body {
+      font-family: "Computer Modern Sans", sans-serif;
+    }
+  </style>
+</head>
+
 <main>
-    <h1>
-        <div class="tooltip">
-        Inline math (static)
-        <span class="tooltiptext">
-            This is to show something {@html math('A = \\pi r^2')}
-        </span>
-        </div>
-    </h1>
+    <div class="title"><a class="atitle" href="https://dx.doi.org/10.4310/JOC.2019.v10.n3.a5">Independence Posets</a></div>
+    <div class="authors">
+        <a class="aauthors" href="http://www.lacim.uqam.ca/~hugh/">Hugh Thomas (UQAM)</a>
+        <p style="display: inline-block; color: #ffcd6f; font-size: 2.66em; margin: 0px; padding: 0px;"> and </p>
+        <a class="aauthors" href="https://personal.utdallas.edu/~nxw170830/index.html">Nathan Williams (UTD)</a>
+      </div>
+          <div class="tagline"><i>"What if a distributive lattice weren't a lattice?"</i></div>
+	
+
+    <div class="writing">
+        <p>
+            Let {@html math('G')} be a finite acyclic directed graph. The transitive closure of {@html math('G')} defines a 
+            <span class="tooltip">poset
+                <span class="tooltiptext">Which we call {@html math('G')}-order</span>
+            </span>.
+            An <span style="color:#004db2"><i>independent set</i></span> {@html math('I \\subseteq G')} is a set of pairwise non-adjacent vertices of {@html math('G')}.</div>
 </main>
