9211e774444f897db1b420a47607c7346dabfb73
NicoOhR <nimoohayonr@gmail.com>
2024-06-02 14:32:38 -0500

    some config updates, SO pull through

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,10 @@
 import { useMathJax } from '$lib/mathjax';
 useMathJax();
 
-let equ = '$$x = \\frac{t}{3}$$';
-
 </script>
 <main>      
     <div class="header"> 
       <h1>Discrete Mathematics</h1>
-      {equ}
     </div>
     <div class="chapter-grid">
       <a class="chapter-link" href="/chapters/chapter1">Chapter 1: The Partially Ordered Set</a>
--- src/routes/chapters/chapter1/+page.svelte
+++ src/routes/chapters/chapter1/+page.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-  import { useMathJax } from '$lib/mathjax';
-  useMathJax();
-  
 </script>
 
 <head>
@@ -10,7 +7,7 @@
     body {
       font-family: "Computer Modern Sans", sans-serif;
     }
-  </style>
+  </style> 
 </head>
 
 <main>
@@ -20,17 +17,15 @@
         <p style="display: inline-block; color: #ffcd6f; font-size: 2.66em; margin: 0px; padding: 0px;"> and </p>
         <a class="aauthors" href="https://personal.utdallas.edu/~nxw170830/index.html">Nathan Williams (UTD)</a>
       </div>
-          <div class="tagline"><i>"What if a distributive lattice weren't a lattice?"</i></div>
-	
+    <div class="tagline"><i>"What if a distributive lattice weren't a lattice?"</i></div>
 
     <div class="row">
       <div class="column left">
-        <p>
-            Let $$G$$ be a finite acyclic directed graph. The transitive closure of $G$ defines a 
+        <p style="display: inline;">
+            Let $G$ be a finite acyclic directed graph. The transitive closure of $G$ defines a 
             <span class="tooltip">poset
                 <span class="tooltiptext">Which we call $G$-order</span>
             </span>.
-            An <span style="color:#004db2"><i>independent set</i></span> $I \subseteq G$ is a set of pairwise non-adjacent vertices of $G$.
-            A pair (<span style="color:#004db2">$D$</span>,<span style="color:#ffcd6f">$U$</span>) of independent sets of $G$ is called <span style="color:#004db2"><i>orthogonal</i></span> if there is no edge in $G$ from an element of $\D$ to an element of $\U$.  An orthogonal pair of independent sets $(\D,\U)$ is called <span style="color:#004db2"><i>tight</i></span> if whenever any element of $\D$ is <div class="tooltip">increased<span class="tooltiptext">removed and replaced by a larger element with respect to $G$-order</span></div> or any element of $\U$ is <div class="tooltip">decreased<span class="tooltiptext">removed and replaced by a smaller element with respect to $G$-order</span></div>, or a new element is added to either $\D$ or $\U$, then the result is no longer an orthogonal pair of independent sets.
-       
+            An <span style="color:#004db2"><i>independent set</i></span> \(I \subseteq G\) is a set of pairwise non-adjacent vertices of $G$.        </div>
+    </div>
 </main>
